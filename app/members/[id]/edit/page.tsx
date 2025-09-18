0c57845a842006e8258dc83764009801e5dbd5f0
青山知史 <aoyamachishi@aoyamachishinoMacBook-Air.local>
2025-09-19 00:18:20 +0900

    新規登録と変種画面を追加

--- app/members/[id]/edit/page.tsx
+++ app/members/[id]/edit/page.tsx
@@ -1,12 +1,7 @@
-// app/members/[id]/edit/page.tsx
 import { getMemberById } from "@/app/lib/data";
 import MemberForm from "../../MemberForm";
 
-export default async function EditMemberPage({
-  params,
-}: {
-  params: { id: string };
-}) {
+export default async function EditMemberPage({ params }: { params: any }) {
   const member = await getMemberById(params.id);
   return <MemberForm mode="edit" initialData={member} memberId={params.id} />;
 }
