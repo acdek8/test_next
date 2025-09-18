d2c3339008936fd69fabc3fa4a42d79ecac06a05
青山知史 <aoyamachishi@aoyamachishinoMacBook-Air.local>
2025-09-19 01:40:46 +0900

    新規登録と変種画面を追加1111

--- app/members/[id]/edit/page.tsx
+++ app/members/[id]/edit/page.tsx
@@ -1,9 +1,11 @@
 import { getMemberById } from "@/app/lib/data";
 import MemberForm from "../../MemberForm";
 
-export default async function EditMemberPage({ params }: { params: any }) {
-  // any なので直接アクセスできます
-  const id = String(params.id);
+export default async function EditMemberPage({ params }: { params?: any }) {
+  // params が Promise<{id: string}> でも {id: string} でも OK
+  const p: { id: string } = await params;
+  const id = String(p.id);
+
   const member = await getMemberById(id);
 
   return <MemberForm mode="edit" initialData={member} memberId={id} />;
