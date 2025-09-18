c466624d67df36419bb6785aa5b96f4dd4d1fa3d
青山知史 <aoyamachishi@aoyamachishinoMacBook-Air.local>
2025-09-19 01:20:58 +0900

    新規登録と変種画面を追加

--- app/members/[id]/edit/page.tsx
+++ app/members/[id]/edit/page.tsx
@@ -1,26 +1,10 @@
 import { getMemberById } from "@/app/lib/data";
 import MemberForm from "../../MemberForm";
 
-ype Params = { id: string };
-
-// Next.js が定義している PageProps の制約に合うように
-// props.params を必ず Promise<Params> として受け取り
-export default async function EditMemberPage({
-  params,
-}: {
-  params: Promise<Params>;
-}) {
-  // await して Params を展開
-  const { id } = await params;
-
-  // 型チェック済みの id を渡してデータ取得
+export default async function EditMemberPage({ params }: { params: any }) {
+  // any なので直接アクセスできます
+  const id = String(params.id);
   const member = await getMemberById(id);
 
-  return (
-    <MemberForm
-      mode="edit"
-      initialData={member}
-      memberId={id}
-    />
-  );
+  return <MemberForm mode="edit" initialData={member} memberId={id} />;
 }
